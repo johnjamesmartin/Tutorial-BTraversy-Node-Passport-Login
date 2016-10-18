6737a3ae58f350b8ab08801d88834ba886ba4caf
“John <johnjamesmartin@live.com>
2016-10-18 02:41:31 +0100

    Part 2 completed

---
+++ models/user.js
@@ -0,0 +1,38 @@
+/* Node modules: */
+
+var mongoose = require('mongoose');
+var bcrypt = require('bcryptjs');
+
+
+/* User schema: */
+
+var UserSchema = mongoose.Schema({
+    username: {
+        type: String,
+        index: true
+    },
+    password: {
+        type: String
+    },
+    email: {
+        type: String
+    },
+    name: {
+        type: String
+    }
+});
+
+
+var User = module.exports = mongoose.model('User', UserSchema);
+
+
+/* Bcrypt to hash password (code from their Github documentation): */
+
+module.exports.createUser = function(newUser, callback) {
+    bcrypt.genSalt(10, function(err, salt) {
+	    bcrypt.hash(newUser.password, salt, function(err, hash) {
+	        newUser.password = hash;
+	        newUser.save(callback);
+	    });
+	});
+}
